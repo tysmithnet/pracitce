43da4339f3d5a6e56f952921cd549a0215ffbee2
Ty Smith <dts5004@gmail.com>
2018-08-11 20:25:30 -0700

    SRM 730 Med

---
+++ SRM 730/ExpectedMinimumPowerDiv2.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+import math,string,itertools,fractions,heapq,collections,re,array,bisect
+
+import operator as op
+def ncr(n, r):
+    r = min(r, n-r)
+    numer = reduce(op.mul, xrange(n, n-r, -1), 1)
+    denom = reduce(op.mul, xrange(1, r+1), 1)
+    return numer//denom
+
+class ExpectedMinimumPowerDiv2:
+    def findExp(self, n, x):
+        max_first  = n - x + 1
+        d = {i:0 for i in xrange(0, max_first + 1)}
+        for i in xrange(1, max_first + 1):
+            d[i] += ncr(n - 1 - (i - 1), x - 1)
+        s = 0
+        for i in xrange(1, max_first + 1):
+            s += ((2 ** i) * d[i])
+                
+        den = ncr(n, x)
+        return s * 1.0 / den
+
+# CUT begin
+# TEST CODE FOR PYTHON {{{
+import sys, time, math
+
+def tc_equal(expected, received):
+    try:
+        _t = type(expected)
+        received = _t(received)
+        if _t == list or _t == tuple:
+            if len(expected) != len(received): return False
+            return all(tc_equal(e, r) for (e, r) in zip(expected, received))
+        elif _t == float:
+            eps = 1e-9
+            d = abs(received - expected)
+            return not math.isnan(received) and not math.isnan(expected) and d <= eps * max(1.0, abs(expected))
+        else:
+            return expected == received
+    except:
+        return False
+
+def pretty_str(x):
+    if type(x) == str:
+        return '"%s"' % x
+    elif type(x) == tuple:
+        return '(%s)' % (','.join( (pretty_str(y) for y in x) ) )
+    else:
+        return str(x)
+
+def do_test(n, x, __expected):
+    startTime = time.time()
+    instance = ExpectedMinimumPowerDiv2()
+    exception = None
+    try:
+        __result = instance.findExp(n, x)
+    except:
+        import traceback
+        exception = traceback.format_exc()
+    elapsed = time.time() - startTime   # in sec
+
+    if exception is not None:
+        sys.stdout.write("RUNTIME ERROR: \n")
+        sys.stdout.write(exception + "\n")
+        return 0
+
+    if tc_equal(__expected, __result):
+        sys.stdout.write("PASSED! " + ("(%.3f seconds)" % elapsed) + "\n")
+        return 1
+    else:
+        sys.stdout.write("FAILED! " + ("(%.3f seconds)" % elapsed) + "\n")
+        sys.stdout.write("           Expected: " + pretty_str(__expected) + "\n")
+        sys.stdout.write("           Received: " + pretty_str(__result) + "\n")
+        return 0
+
+def run_tests():
+    sys.stdout.write("ExpectedMinimumPowerDiv2 (500 Points)\n\n")
+
+    passed = cases = 0
+    case_set = set()
+    for arg in sys.argv[1:]:
+        case_set.add(int(arg))
+
+    with open("ExpectedMinimumPowerDiv2.sample", "r") as f:
+        while True:
+            label = f.readline()
+            if not label.startswith("--"): break
+
+            n = int(f.readline().rstrip())
+            x = int(f.readline().rstrip())
+            f.readline()
+            __answer = float(f.readline().rstrip())
+
+            cases += 1
+            if len(case_set) > 0 and (cases - 1) in case_set: continue
+            sys.stdout.write("  Testcase #%d ... " % (cases - 1))
+            passed += do_test(n, x, __answer)
+
+    sys.stdout.write("\nPassed : %d / %d cases\n" % (passed, cases))
+
+    T = time.time() - 1534036764
+    PT, TT = (T / 60.0, 75.0)
+    points = 500 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT))
+    sys.stdout.write("Time   : %d minutes %d secs\n" % (int(T/60), T%60))
+    sys.stdout.write("Score  : %.2f points\n" % points)
+
+if __name__ == '__main__':
+    run_tests()
+
+# }}}
+# CUT end
